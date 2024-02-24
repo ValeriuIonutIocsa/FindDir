5f0aa0b6de10431e98655405301801c13086f6fe
Iocsa, Valeriu-Ionut <valeriu.iocsa@vitesco.com>
2024-02-24 18:44:42 +0200

    updated dependencies

--- Projects/Personal/FindDir/FindDir/src/test/java/com/personal/scripts/gen/find_dir/AppStartFindDirTest.java
+++ Projects/Personal/FindDir/FindDir/src/test/java/com/personal/scripts/gen/find_dir/AppStartFindDirTest.java
@@ -2,19 +2,21 @@ package com.personal.scripts.gen.find_dir;
 
 import org.junit.jupiter.api.Test;
 
+import com.utils.test.TestInputUtils;
+
 class AppStartFindDirTest {
 
 	@Test
 	void testMain() {
 
 		final String[] args;
-		final int input = Integer.parseInt("1");
+		final int input = TestInputUtils.parseTestInputNumber("1");
 		if (input == 1) {
 
 			args = new String[] {
-                    "C:\\IVI\\",
-                    ".*\\\\io-utils\\\\io-utils"
-            };
+					"C:\\IVI\\",
+					".*\\\\io-utils\\\\io-utils"
+			};
 
 		} else {
 			throw new RuntimeException();
